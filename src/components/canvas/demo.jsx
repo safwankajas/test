f11fad3c8799b297c70c7bd9b75f461a3d39ea97
safwankajas <84768183+safwankajas@users.noreply.github.com>
2023-04-05 00:23:35 +0530

    src_uploaded&&all

---
+++ src/components/canvas/demo.jsx
@@ -0,0 +1,13 @@
+import { OrbitControls } from '@react-three/drei'
+import Watch from './Watch'
+
+const Demo = () => {
+  return (
+    <>
+    <OrbitControls/>
+    <Watch/>
+    </>
+  )
+}
+
+export default Demo
---
+++ src/components/canvas/index.js
@@ -0,0 +1,7 @@
+import EarthCanvas from "./Earth";
+import BallCanvas from "./Ball";
+import ComputersCanvas from "./Computers";
+import Demo from "./demo";
+import StarsCanvas from "./Stars";
+
+export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas,Demo };
