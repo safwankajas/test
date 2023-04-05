f80cf3e0aa9fea2a606954342c4da718aa216e73
safwankajas <84768183+safwankajas@users.noreply.github.com>
2023-04-06 00:39:33 +0530

    3d cmptr

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,7 @@ import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
   
-  const computer = useGLTF("./desktop_pc/watch.glb");
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh >
